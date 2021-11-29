8012361777b810b94f59c92a1e022513d3d43264
matecolombo <mateo.colombo95@gmail.com>
2021-11-28 21:44:16 -0300

    Se agregan edificion y ciudades

---
+++ src/main/java/edu/fiuba/algo3/modelo/Montreal.java
@@ -0,0 +1,21 @@
+package edu.fiuba.algo3.modelo;
+
+import edu.fiuba.algo3.modelo.edificios.Aeropuerto;
+import edu.fiuba.algo3.modelo.edificios.Banco;
+import edu.fiuba.algo3.modelo.edificios.Bolsa;
+import edu.fiuba.algo3.modelo.edificios.Puerto;
+
+public class Montreal {
+
+    Banco banco;
+    Aeropuerto aeropuerto;
+    Puerto puerto;
+    Bolsa bolsa;
+    protected void recibirJugador(Jugador jugador) {
+
+    }
+
+    protected String mostrarPista() {
+        return Pista;
+    }
+}
