55162d5c8443498ff3ff7351339700a251abe1cf
matecolombo <mateo.colombo95@gmail.com>
2021-11-28 22:09:00 -0300

    Se agregan edificion y ciudade

---
+++ src/main/java/edu/fiuba/algo3/modelo/ciudades/Ciudad.java
@@ -0,0 +1,17 @@
+package edu.fiuba.algo3.modelo.ciudades;
+
+import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.edificios.*;
+
+import java.util.List;
+
+public class Ciudad {
+
+    String nombre;
+
+    List<Edificio> edificios;
+
+    protected void recibirJugador(Jugador jugador) {
+        /* TODO */
+    }
+}
