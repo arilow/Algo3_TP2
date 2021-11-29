778f3b75a8a81a4bca44f97977a73f9724976e95
Mateo <mateo.colombo95@gmail.com>
2021-11-29 19:12:35 -0300

    pequeñas correcciones en clases ciudad y edificio

--- src/main/java/edu/fiuba/algo3/modelo/ciudades/Ciudad.java
+++ src/main/java/edu/fiuba/algo3/modelo/ciudades/Ciudad.java
@@ -8,9 +8,7 @@ import java.util.List;
 public class Ciudad {
 
     String nombre;
-
     List<Edificio> edificios;
-
     protected void recibirJugador(Jugador jugador) {
         /* TODO */
     }
