0a0a8b41e421063dc5105fd2383a08d63b8ba463
Dogui96 <arribas.guido@mercadolibre.com>
2021-11-28 21:17:40 -0300

    Starting models

---
+++ src/main/java/edu/fiuba/algo3/modelo/Juego.java
@@ -0,0 +1,13 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.List;
+
+public class Juego {
+    protected final List<String> datosDeCiudades;
+    protected final List<String> datosDeLadrones;
+
+    public iniciarMenu() {
+        this.esContent = esContent;
+        this.usContent = usContent;
+    }
+}
