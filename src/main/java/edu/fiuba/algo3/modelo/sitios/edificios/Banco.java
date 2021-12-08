135043ed8973dc9084252b63b1f350a25f670787
Dogui96 <arribas.guido@mercadolibre.com>
2021-12-08 13:04:22 -0300

    limpiando cosas

--- src/main/java/edu/fiuba/algo3/modelo/sitios/edificios/Banco.java
+++ src/main/java/edu/fiuba/algo3/modelo/sitios/edificios/Banco.java
@@ -3,9 +3,6 @@ package edu.fiuba.algo3.modelo.sitios.edificios;
 import edu.fiuba.algo3.modelo.Jugador;
 
 public class Banco extends Edificio{
-
-    private Jugador jugador;
-
     public Banco(String pista) {
         super(pista);
     }
